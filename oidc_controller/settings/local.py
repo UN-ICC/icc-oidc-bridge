a2c3d06b71251b806fcc5997c89e2402a717c30c
lohanspies <21362987+lohanspies@users.noreply.github.com>
2021-04-07 23:38:02 +0200

    changes to get it running locally

--- oidc_controller/settings/local.py
+++ oidc_controller/settings/local.py
@@ -34,10 +34,10 @@ SITE_URL = os.environ["SITE_URL"]
 
 JWT_TOKEN_VALIDITY = 720
 JWT_TOKEN_ISSUER = SITE_URL
-JWT_KEY_KID = "3fd0079d2f87421708864ee9c84c8d4d"
+JWT_KEY_KID = "525786059b724b15b2d23033664342ff" # Make sure this is the kid that matches the RSA keypair generated in the OIDC admin interface
 
 
 ACA_PY_URL = f"http://aca-py:{ACAPY_ADMIN_PORT}"
-ACA_PY_TRANSPORT_URL = f"http://aca-py:{ACAPY_TRANSPORT_PORT}"
+ACA_PY_TRANSPORT_URL = f"http://994143a56d38.ngrok.io" # Make sure this is an ngrok endpoint i.e. ngrok http http://localhost:5679
 POLL_INTERVAL = 5000
 POLL_MAX_TRIES = 12
