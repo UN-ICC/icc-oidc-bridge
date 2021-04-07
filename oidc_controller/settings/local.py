e8ead6fff36cb501ffc93fd4571de2492cb3078d
lohanspies <lohan.spies@didx.co.za>
2021-04-08 00:43:34 +0200

    updated ngrok endpoints

--- oidc_controller/settings/local.py
+++ oidc_controller/settings/local.py
@@ -38,6 +38,7 @@ JWT_KEY_KID = "525786059b724b15b2d23033664342ff" # Make sure this is the kid tha
 
 
 ACA_PY_URL = f"http://aca-py:{ACAPY_ADMIN_PORT}"
-ACA_PY_TRANSPORT_URL = f"http://994143a56d38.ngrok.io" # Make sure this is an ngrok endpoint i.e. ngrok http http://localhost:5679
+# Make sure this is an ngrok endpoint i.e. ngrok http http://localhost:5679
+ACA_PY_TRANSPORT_URL = f"https://5e2594552eaf.ngrok.io"
 POLL_INTERVAL = 5000
 POLL_MAX_TRIES = 12
