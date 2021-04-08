8a533177cca51e13464894ef84214ea9b6e1f136
lohanspies <lohan.spies@didx.co.za>
2021-04-08 02:11:42 +0200

    fix variable name

--- oidc_controller/settings/local.py
+++ oidc_controller/settings/local.py
@@ -37,7 +37,7 @@ JWT_TOKEN_ISSUER = SITE_URL
 JWT_KEY_KID = "525786059b724b15b2d23033664342ff" # Make sure this is the kid that matches the RSA keypair generated in the OIDC admin interface
 
 
-ACA_PY_URL = f"http://aca-py:{ACAPY_ADMIN_PORT}"
+ACA_PY_URL = f"http://aca-py:{ACA_PY_ADMIN_PORT}"
 # Make sure this is an ngrok endpoint i.e. ngrok http http://localhost:5679
 ACA_PY_TRANSPORT_URL = f"https://5e2594552eaf.ngrok.io"
 POLL_INTERVAL = 5000
